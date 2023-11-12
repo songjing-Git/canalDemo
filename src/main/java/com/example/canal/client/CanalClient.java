a0d46d258b644ca7b663cbc26f76ebedebe7d353
sj <1249932920@qq.com>
2023-11-12 22:36:28 +0800

    canal

--- src/main/java/com/example/canal/client/CanalClient.java
+++ src/main/java/com/example/canal/client/CanalClient.java
@@ -3,18 +3,25 @@ package com.example.canal.client;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.google.protobuf.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class CanalClient {
 
     private final Logger logger = LoggerFactory.getLogger(CanalClient.class);
 
-    public  void run() {
+    private final List<CanalEntry.EventType> list = Arrays.asList(CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE, CanalEntry.EventType.UPDATE);
+
+    public void run() throws Exception {
         String ip = "110.40.128.13";
         String destination = "example";
         //创建连接对象
@@ -22,23 +29,51 @@ public class CanalClient {
                 new InetSocketAddress(ip, 11111), destination, "", ""
         );
         canalConnector.connect();
-        canalConnector.subscribe(".*\\\\..*");
+        canalConnector.subscribe("canal.canal_test01");
         canalConnector.rollback();
-        int batchSize = 1;
-        try {
+
+        while (true) {
             com.alibaba.otter.canal.protocol.Message message = canalConnector.getWithoutAck(1);
-            long batchId = message.getId();
-            int size = message.getEntries().size();
-            if (batchId == -1 || size == 0) {
-                logger.debug("message : {}", message);
+            long id = message.getId();
+            List<CanalEntry.Entry> entries = message.getEntries();
+            //判空
+            if (CollectionUtils.isEmpty(entries)) {
+                logger.info(" entries is null ");
                 Thread.sleep(1000);
-            } else {
-                logger.debug("message : {}", message);
+                continue;
             }
-            canalConnector.ack(batchId);
-            //当队列里面堆积的sql大于一定数值的时候就模拟执行
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            for (CanalEntry.Entry entry : entries) {
+                String tableName = entry.getHeader().getTableName();
+
+                CanalEntry.EntryType entryType = entry.getEntryType();
+
+                ByteString storeValue = entry.getStoreValue();
+
+                if (entryType == CanalEntry.EntryType.ROWDATA) {
+
+                    CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(storeValue);
+                    //获取操作类型
+                    CanalEntry.EventType eventType = rowChange.getEventType();
+                    if (list.contains(eventType)) {
+                        //拿到行数据
+                        List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
+
+                        for (CanalEntry.RowData rowData : rowDatasList) {
+                            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+                            logger.info("tableName is {} row data is {}", tableName, rowData);
+                            for (CanalEntry.Column column : beforeColumnsList) {
+                                logger.info(" before data is  name {} value is {}", column.getName(), column.getValue());
+                            }
+
+                            List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+                            for (CanalEntry.Column column : afterColumnsList) {
+                                logger.info(" after data is  name {} value is {}", column.getName(), column.getValue());
+                            }
+                        }
+                    }
+                }
+            }
+            canalConnector.ack(id);
         }
     }
 }
